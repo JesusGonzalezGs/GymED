e4582db3236d6d8358dda3557a1b7c8c71b62709
ukanda-works <manuel.amoros.egidio@gmail.com>
2022-01-25 15:52:22 +0100

    sql_2

---
+++ sql/conectar_bd.php
@@ -0,0 +1,10 @@
+<?php  
+DEFINE("DB_HOST", "localhost");  
+DEFINE("DB_USER", "gymed");  
+DEFINE("DB_PASSWORD", "gymed");  
+DEFINE("DB_NAME", "GYMED");  
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);  
+if (!$con || !mysqli_select_db($con,DB_NAME)) {    
+die("Error conectando a la BD: " . mysqli_error($con));   
+} 
+?>
