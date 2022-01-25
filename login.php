a5b3b89d56bd5a6f938a90d80a2f032ed1b6825e
ukanda-works <manuel.amoros.egidio@gmail.com>
2022-01-25 16:49:39 +0100

    login back

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <legend>Introduce los datos de tu usuario:</legend>
+        </br>
+        <legend>Información de usuario:</legend>
+        </br>
+        <label for="usuario">Usuario<label>
+        <input type="text" id="usuario" name="usuario">
+        </br>
+        <label for="contraseña">Contraseña</label>
+        <input type="password" id="contraseña" name="password">
+        <input type="submit" value="Enviar">
+  </body>
+</html>
