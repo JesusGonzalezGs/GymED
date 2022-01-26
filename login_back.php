7e8876bf82b2c23ba6d41d556876935722e96796
ukanda-works <manuel.amoros.egidio@gmail.com>
2022-01-26 16:05:49 +0100

    login back funcionando +-

--- login_back.php
+++ login_back.php
@@ -1,5 +1,5 @@
 <?php
-include("conectar_bd.php");
+include("sql/conectar_bd.php");
 
 //pillamos el usuario y la contraseña introducidos
 $user = $_GET['user'];
@@ -7,9 +7,23 @@ $passwd = $_GET['passwd'];
 //pasamos la contraseña por el md5  para comparar despues
 $passwd = md5($passwd);
 
-//sentencia sql
+/*comprobacion de conexcion descomentar en caso de error
+if ($con->connect_errno) {
+    printf("Falló la conexión: %s\n", $mysqli->connect_error);
+    exit();
+}else{
+    echo "conexion ok";
+}*/
+//sentencia y realizacion de conexion;
 $sentencia = "SELECT hash_pwd FROM usuarios where nombre = \"$user\"";
 $respuesta = mysqli_query($con,$sentencia);
 
-echo $respuesta;
+while ( $rows = $respuesta->fetch_assoc() ) {
+    $hash= $rows[hash_pwd];
+}
+if ($passwd == $hash){
+    echo "logueado correctamente";
+}
+/* comprobar la conexión */
+
 ?>
