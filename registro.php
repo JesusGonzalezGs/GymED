43274ef6a6adab5c9846405fd905a5fa3b20a850
ukanda-works <manuel.amoros.egidio@gmail.com>
2022-01-27 17:30:14 +0100

    creacion funcion de pie de pagina

--- registro.php
+++ registro.php
@@ -88,67 +88,6 @@ session_start();
 
 
 <!-- Footer -->
-<footer class="page-footer font-small cyan lighten-5">
-
-  <div class="container text-center text-md-left mt-5">
-
-    <div class="row mt-3 dark-grey-text">
- 
-      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-        <h6 class="text-uppercase font-weight-bold">Enlaces</h6>
-        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-        <p>
-          <a class="dark-grey-text" href="#!">Inicio</a>
-        </p>
-        <p>
-          <a class="dark-grey-text" href="#!">Servicios</a>
-        </p>
-        <p>
-          <a class="dark-grey-text" href="#!">Productos</a>
-        </p>
-        <p>
-          <a class="dark-grey-text" href="#!">Actividades</a>
-        </p>
-
-      </div>
-
-      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-        <h6 class="text-uppercase font-weight-bold">Enlaces</h6>
-        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-        <p>
-          <a class="dark-grey-text" href="#!">Tu perfil</a>
-        </p>
-        <p>
-          <a class="dark-grey-text" href="#!">Registrate!</a>
-        </p>
-        <p>
-          <a class="dark-grey-text" href="#!">Reservas</a>
-        </p>
-        <p>
-          <a class="dark-grey-text" href="#!">Ayuda</a>
-        </p>
-
-      </div>
- 
-      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
-        <h6 class="text-uppercase font-weight-bold">Contactanos</h6>
-        <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-        <p>
-          <a href="https://www.google.com/maps/place/IES+Severo+Ochoa+Elche/@38.2799275,-0.7161222,15z/data=!4m2!3m1!1s0x0:0x609104ad972f51c4?sa=X&ved=2ahUKEwii-YGyydH1AhVKPBoKHRlJBlMQ_BJ6BAghEAU"><i class="fas fa-home mr-3"></i> Elche, 03203, España</p></a>
-        <p>
-          <i class="fas fa-envelope mr-3"></i> contact@gymed.es</p>
-        <p>
-          <i class="fas fa-phone mr-3"></i> +34 666 999 666</p>
-
-      </div>
-    </div>
-    <div class="footer-copyright text-center py-3">
-      <p>GymEd empresa ficticia, sin registro fiscal</p>
-  </div>
-  </div>
-</footer>
+<?php footer();?>
 
 </html>
