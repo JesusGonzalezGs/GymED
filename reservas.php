75f3087a54ffa559da23bcb1e068b5a00f43eb0c
JesusGonzalezGs <jesusgonzalezg2003@gmail.com>
2022-02-02 10:57:54 +0100

    commit favicon añadido

--- reservas.php
+++ reservas.php
@@ -16,6 +16,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gimnasio ED</title>
+    <link rel="shortcut icon" href="src/img/LOGO_SMALL.ico">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <script src="js/jquery-3.6.0.min.js"></script>
