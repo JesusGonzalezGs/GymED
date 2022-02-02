83fd08708a9520f925f2bfddc6542a451ad6d98c
ukanda-works <manuel.amoros.egidio@gmail.com>
2022-02-02 12:08:16 +0000

    actualizacion BDD

--- sql/mysql_gymed.sql
+++ sql/mysql_gymed.sql
@@ -1,11 +1,8 @@
 -- cuando se vaya a actualizar primero se ejecuta el drop
 drop database if exists GYMED;
-
 -- esto es el grueso
 create database GYMED;
-
 use GYMED;
-
 create table usuarios(
 id int primary key not null auto_increment,
 correo varchar(50) unique,
@@ -14,9 +11,9 @@ apellidos varchar(40) ,
 hash_pwd varchar(200),
 dni varchar(9) unique
 );
--- los usuarios de prueba sera admin y usuario, sus pass son igual que sus nombres
+
 insert into usuarios (correo,nombre, apellidos, hash_pwd,dni) values
-("admin@admin.com","admin","","21232f297a57a5a743894a0e4a801fc3","99999999Y"), 
+("admin@admin.com","admin","","21232f297a57a5a743894a0e4a801fc3","99999999Y"),
 ("usuario@usuario.com","usuario","usuario","f8032d5cae3de20fcec887f395ec9a6a","11111111Y");
 
 	-- nombre,actividades, apellido, id, dni, hash...
@@ -27,16 +24,42 @@ descripcion varchar(100),
 plazas_disponibles int not null
 );
 
+insert into actividades(nombre,descripcion,plazas_disponibles) values
+("zumba","dale a tu cuerpo lo que se merece con nuestro profesor de zumba el doctor zumbao ","20"),
+("Spining","es como hacer ciclismo pero sin moverte. Que puede salir mal?","30"),
+("Prama","no se lo que es pero suena a algo sano que seguramente te venga bien","10"),
+("pilates","Eres un padre tinerfenyo, divorciado, miope? Entonces esta actividad es para ti","25"),
+("Body combat","body combat","16");
 -- nombre, id, dias, plazas disponibles....
 
 create table complementos(
 id int primary key not null auto_increment,
 nombre varchar(30) not null,
+tipo varchar(30) not null,
 precio decimal not null,
 stock int,
 descripcion varchar(100)
 );
 
+insert into complementos(nombre, precio,descripcion, stock,  tipo) values
+("proteinas","35","proteinas","11","chocolate"),
+("proteinas","35","proteinas","11","vainilla"),
+("proteinas","35","proteinas","11","oreo"),
+("barritas","1.5","barritas","11","chocolate"),
+("barritas","1.5","barritas","11","caramelo"),
+("barritas","1.5","barritas","11","cereales"),
+("bebida energetica","2","bebidas energeticas","11","mango"),
+("bebida energetica","2","bebidas energeticas","11","red bull"),
+("bebida energetica","2","bebidas energeticas","11","lima"),
+("BCA's","15","BCA's","11","cereza"),
+("BCA's","15","BCA's","11","neutro"),
+("BCA's","15","BCA's","11","pinya"),
+("Aminoacidos","20","Aminoacidos","11","frutas del bosque"),
+("Aminoacidos","20","Aminoacidos","11","manzana"),
+("Aminoacidos","20","Aminoacidos","11","mango"),
+("Creatina ","25","Creatina ", "11", "cereza"),
+("Creatina ","25","Creatina ", "11", "neutra"),
+("Creatina ","25","Creatina ", "11", "lima");
 -- nombre, id , precio, stock(unidades), descripcion...
 
 
